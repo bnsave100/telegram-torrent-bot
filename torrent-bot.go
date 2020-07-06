21b41bc70f64d6000a9ba3a2b59c3536f39e3135
Ruben Iritsyan <treyan94@gmail.com>
2020-07-06 16:56:33 +0400

    qBittorrent init

--- torrent-bot.go
+++ torrent-bot.go
@@ -5,18 +5,26 @@ import (
 	"fmt"
 	"gopkg.in/tucnak/telebot.v2"
 	"log"
+	"strings"
 	"time"
+	"torrent-bot/qbittorrent"
 )
 
 var telegramUserId int
+var qBittorrentClient *qbittorrent.QBittorrent
 
 func main() {
 	keyFlag := flag.String("key", "", "telegram api key")
 	userIdFlag := flag.Int("user", 0, "telegram user id")
+	qblFlag := flag.String("qbl", "", "qBittorrent username")
+	qbpFlag := flag.String("qbp", "", "qBittorrent password")
+	qbuFlag := flag.String("qbu", "", "qBittorrent base url")
 
 	flag.Parse()
 
 	telegramUserId = *userIdFlag
+	qBittorrentClient = qbittorrent.NewQBittorrent(*qblFlag, *qbpFlag, *qbuFlag)
+
 	bot := getBot(*keyFlag)
 
 	bot.Handle("/add", add)
