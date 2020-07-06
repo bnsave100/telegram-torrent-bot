e2d891739881985821bd8350cddd68ea4783b29d
Ruben Iritsyan <treyan94@gmail.com>
2020-07-06 16:43:22 +0400

    code improvements

--- torrent-bot.go
+++ torrent-bot.go
@@ -10,29 +10,15 @@ import (
 
 var telegramUserId int
 
-var bot = func() (bot *telebot.Bot) {
+func main() {
 	keyFlag := flag.String("key", "", "telegram api key")
 	userIdFlag := flag.Int("user", 0, "telegram user id")
 
 	flag.Parse()
 
 	telegramUserId = *userIdFlag
+	bot := getBot(*keyFlag)
 
-	bot, err := telebot.NewBot(telebot.Settings{
-		Token: *keyFlag,
-		Poller: &telebot.LongPoller{
-			Timeout: 10 * time.Second,
-		},
-	})
-
-	if err != nil {
-		log.Fatal("Error connecting to telegram, please check your api key.")
-	}
-
-	return bot
-}()
-
-func main() {
 	bot.Handle("/add", add)
 
 	bot.Start()
@@ -44,4 +30,20 @@ func add(m *telebot.Message) {
 	}
 
 	fmt.Printf("%+v/n", m)
+}
+
+func getBot(apiKey string) (bot *telebot.Bot) {
+
+	bot, err := telebot.NewBot(telebot.Settings{
+		Token: apiKey,
+		Poller: &telebot.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	})
+
+	if err != nil {
+		log.Fatal("Error connecting to telegram, please check your api key.")
+	}
+
+	return bot
 }
